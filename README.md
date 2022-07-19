f9173ef68185f72c1d1a153fe45d0d6785895a40
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-19 09:58:25 -0500

    Type Guards

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TypeScript-Advanced-Concepts
+
+## Type Guards
+
+Validates whether or not a method can be used on a type. Allows for a method validation. Working with object use `if in` or `instanceof`. Working with other DataTypes use `typeof` check.
--- src/app.ts
+++ src/app.ts
@@ -18,3 +18,53 @@ const e1: ElevatedEmployee = {
 type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
+
+// Type Guards - typeof!
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+// Type Guards - if in!
+type UnknownEmployee = Employee | Admin;
+function printEmployeeInfo(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInfo({ name: "Manuel", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+class Truck {
+  drive() {
+    console.log("Trucking...");
+  }
+  loadCargo(amount: number) {
+    console.log("Loading cargo..." + amount);
+  }
+}
+type Vehicle = Car | Truck;
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  //   if ("loadCargo" in vehicle) {
+  //     vehicle.loadCargo(10000);
+  //   }
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(10000);
+  }
+}
+useVehicle(v1);
+useVehicle(v2);
